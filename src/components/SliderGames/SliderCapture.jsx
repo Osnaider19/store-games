146a159f16366690e5a331ef76d73b7f0adae956
osnaider <osnaidermartinez2002@gmail.com>
2023-07-26 21:08:33 -0500

    slider games

--- src/components/SliderGames/SliderCapture.jsx
+++ src/components/SliderGames/SliderCapture.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Button } from "./Button";
 import "./index.css";
+import { useRef } from "react";
+import { handelScrollLeft, handelScrollRigth } from "./FuntionScroll";
 export const SliderCapture = ({ capture }) => {
+  const refSliderCapture = useRef();
   return (
     <>
-      <div className="relative w-full  bg-red-400 content-img-capture">
-        <Button />
-        <div className="relative w-full flex  flex-nowrap max-w-[300px] h-[310px] overflow-hidden overflow-x-scroll snap-mandatory  transition-all duration-200 ">
+      <div className="relative w-full  content-img-capture">
+        <Button
+          scrollLeft={() => {
+            handelScrollLeft(refSliderCapture);
+          }}
+          scrollRigth={() => {
+            handelScrollRigth(refSliderCapture);
+          }}
+        />
+        <div
+          className="relative w-full flex  flex-nowrap max-w-[300px] h-[310px] scroll-smooth snap-mandatory snap-x overflow-hidden transition-all duration-200 "
+          ref={refSliderCapture}
+        >
           {capture?.map((capture) => (
-            <div className="min-w-full min-h-full" key={capture.id}>
+            <div className="relative min-w-full min-h-full snap-center" key={capture.id}>
               <img
                 loading="lazy"
-                src={capture.image}
+                src={capture.image.replace("media/", `media/crop/600/400/`)}
                 alt=""
                 className="w-full min-w-full h-full object-cover"
               />
