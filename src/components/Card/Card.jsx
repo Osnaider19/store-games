ba51dfd3a5eccce9a4ed42795d0bb79160187083
osnaider <osnaidermartinez2002@gmail.com>
2023-07-30 22:28:52 -0500

    modific card new style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,17 +6,19 @@ import { GenresGames } from "../SliderGames/GenresGames";
 import { Link } from "react-router-dom";
 export const Card = ({ capture, name, id, star, genres, date }) => {
   return (
-    <div>
+    <div className="pb-7 border rounded-lg">
       <SliderCapture capture={capture} />
-      <div>
-        <Link to={`/games/${id}`} className="font-semibold hover:opacity-80">
-          <p className="text-xl py-1">{name}</p>
-        </Link>
-      </div>
-      <Star rating={star} />
-      <GenresGames genres={genres} />
-      <div>
-        <p>{convertirFecha(date)}</p>
+      <div className="px-2">
+        <div>
+          <Link to={`/games/${id}`} className="font-semibold hover:opacity-80">
+            <p className="text-xl py-1">{name}</p>
+          </Link>
+        </div>
+        <Star rating={star} />
+        <GenresGames genres={genres} />
+        <div>
+          <p>{convertirFecha(date)}</p>
+        </div>
       </div>
     </div>
   );
