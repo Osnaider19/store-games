7848e43a8e371324e75f06fa3b1f749111cb64d4
osnaider <osnaidermartinez2002@gmail.com>
2023-08-02 22:08:34 -0500

    RatingStar new font zize auto

--- src/components/Slider/RatingStar.jsx
+++ src/components/Slider/RatingStar.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { HiStar } from "react-icons/hi";
-export const RatingStar = ({ rating, votes }) => {
+export const RatingStar = ({ rating }) => {
   return (
     <div className="relative py-1">
-      <p className="font-semibold">Calificación</p>
       <div className="text-2xl"></div>
       <div className="flex ">
         {[...new Array(5)].map((star, index) => {
           return index < Math.floor(rating) ? (
-            <i className=" text-yellow-400 text-2xl" key={index}>
+            <i className="text-yellow-400" key={index}>
               <HiStar />
             </i>
           ) : (
-            <i className=" text-white text-2xl" key={index}>
+            <i className=" text-white" key={index}>
               <HiStar />
             </i>
           );
