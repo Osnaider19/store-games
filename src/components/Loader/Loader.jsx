991312a30e5e12d0be79695e5f8e0fa961911440
osnaider <osnaidermartinez2002@gmail.com>
2023-08-10 18:31:56 -0500

    add loaders

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './index.css'
+export const Loader = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <span className="loader"></span>
+    </div>
+  );
+};
