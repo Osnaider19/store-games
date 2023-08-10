9e4f21ac318929dbbd8c17ec34cb3eb290fc7f02
osnaider <osnaidermartinez2002@gmail.com>
2023-08-10 18:36:42 -0500

    add context all games

---
+++ src/Context/ContextGames/ContextGames.jsx
@@ -0,0 +1,52 @@
+import { createContext, useEffect, useState } from "react";
+import { API_KEY, URL } from "../../config/config";
+export const ContextGames = createContext();
+
+export const ContextGamesProvider = ({ children }) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [date, setDate] = useState("");
+
+  const getData = async (url) => {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw {
+          err: true,
+          status: res.status,
+          statusText: !res.statusText ? "Ocurrió un error" : res.statusText,
+        };
+      }
+      const data = await res.json();
+      setData(data);
+      setError({ err: false });
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+  useEffect(() => {
+    getData(
+      `${URL}/games?key=${API_KEY}&dates=${date}&page=${page}&page_size=40`
+    );
+  }, [page, date]);
+  return (
+    <ContextGames.Provider
+      value={{
+        data,
+        isPending,
+        setIsPending,
+        setPage,
+        setDate,
+        date,
+        page,
+        error,
+      }}
+    >
+      {children}
+    </ContextGames.Provider>
+  );
+};
