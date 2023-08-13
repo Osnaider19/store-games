4018c12a72799db382fa19945164d150d55734d6
osnaider <osnaidermartinez2002@gmail.com>
2023-08-12 22:28:50 -0500

    add  requirements in details games

---
+++ src/components/GamesDetails/Requirements.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { ContextDetails } from "../../Context/contextDetails/ContextDetails";
+
+export const Requirements = () => {
+  const { data } = useContext(ContextDetails);
+  return (
+    <div className="relative flex justify-start items-center px-8 w-full py-4">
+      {console.log(data)}
+      <div>
+        {data.platforms?.map((plat) => (
+          <div key={plat.platform.id}>
+            {plat.requirements && (
+              <>
+                <div className="text-xl pb-2 font-semibold">
+                  System requirements for {plat.platform.name}
+                </div>
+                <div>
+                  {plat.requirements.minimum && (
+                    <div>
+                      <p className="font-semibold text-xl">Minimum:</p>
+                      <p> {plat.requirements.minimum}</p>
+                    </div>
+                  )}
+                  {plat.requirements.recommended && (
+                    <div>
+                      <p className="font-semibold text-xl">Recommended:</p>
+                      <p>{plat.requirements.recommended}</p>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
