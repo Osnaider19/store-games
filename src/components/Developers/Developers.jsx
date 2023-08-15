b7d74e7e5d269ab6498fc8814e4c2450c65df38b
osnaider <osnaidermartinez2002@gmail.com>
2023-08-14 21:24:25 -0500

    add pagination in developers

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -1,29 +1,37 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { API_KEY, URL } from "../../config/config";
 import { useFetch } from "../../hooks/useFech";
 import { Card } from "../Card/Card";
 import { Loader } from "../Loader/Loader";
+import { useState, useEffect } from "react";
+import { Pagination } from "../Pagination/Pagination";
+import { NoResults } from "../Games/NoResults";
+import { Error } from "../Errors/Error";
 export const Developers = () => {
   const { name } = useParams();
-  const { data, error, isPending, setIsPending } = useFetch(
-    `${URL}/developers/${name}?key=${API_KEY}`
+  const [page, setPage] = useState(1);
+  const { data, isPending, setIsPending, error } = useFetch(
+    `${URL}/games?key=${API_KEY}&developers=${name}&page=${page}`
   );
-  const { data: games, isPending: isPendinggames } = useFetch(
-    `${URL}/games?key=${API_KEY}&developers=${name}`
-  );
+  useEffect(() => {
+    scrollTo(0, 0);
+  }, [page]);
   return (
     <>
       {console.log(data)}
-      {console.log(games)}
+
       {data && (
-        <div className="pt-[60px] px-8 py-3">
+        <div className="pt-[60px] px-8 py-5">
           <div>
-            <h1 className="text-5xl font-semibold">Developed by {data.name}</h1>
+            <h1 className="text-5xl font-semibold capitalize pt-7">
+              Developed by {name.replace(/-/g, " ")}
+            </h1>
           </div>
-          {isPendinggames && <Loader />}
+          {isPending && <Loader />}
+          {data?.results?.length <= 0 && <NoResults />}
+          {error && <Error status={error.status} statusText={error.statusText} />}
           <div className="flex flex-wrap w-full gap-3 justify-between py-10">
-            {games?.results?.map((game) => (
+            {data?.results?.map((game) => (
               <div
                 className="relative  max-w-[250px] h-full min-w-[250px]  overflow-hidden hover:-translate-y-3 transition-transform duration-200"
                 key={game.id}
@@ -39,6 +47,13 @@ export const Developers = () => {
               </div>
             ))}
           </div>
+          <Pagination
+            next={data?.next}
+            page={page}
+            previous={data?.previous}
+            setIsPending={setIsPending}
+            setPage={setPage}
+          />
         </div>
       )}
     </>
