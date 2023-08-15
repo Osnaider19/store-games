22e1d1254545c109bc37c38501a4e5a02122615c
osnaider <osnaidermartinez2002@gmail.com>
2023-08-14 21:38:53 -0500

    add pagination y filters

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,35 +1,52 @@
-import React, { useEffect } from "react";
-import { Layout } from "../Layout/Layout";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFech";
 import { URL } from "../../config/config";
 import { API_KEY } from "../../config/config";
 import { Card } from "../Card/Card";
-import { LoaderCard } from "../Loader/LoaderCard";
+import { Loader } from "../Loader/Loader";
+import { useState, useEffect } from "react";
+import { Pagination } from "../Pagination/Pagination";
+import { Footer } from "../Footer/Footer";
+import { Filters } from "../Filters/Filter";
+import { NoResults } from "../Games/NoResults";
 export const Genres = () => {
   const { name } = useParams();
-  const { data , error , isPending , setIsPending } = useFetch(`${URL}/games?key=${API_KEY}&genres=${name}`);
+  const [page, setPage] = useState(1);
+  const [date, setDate] = useState("");
+  const [ordering, setOrdering] = useState("");
+  const { data, error, isPending, setIsPending } = useFetch(
+    `${URL}/games?key=${API_KEY}&genres=${name}&page=${page}&dates=${date}&ordering=${ordering}`
+  );
 
   useEffect(() => {
     scrollTo(0, 0);
-    setIsPending(true)
-  }, [name]);
+    setIsPending(true);
+  }, [name, page]);
   return (
-
-    <Layout>
+    <>
       {console.log(data)}
-      
-      <div className="px-5">
-        <h1 className="py-3 text-4xl font-bold">Juegos de {name}</h1>
-        {isPending && <LoaderCard/>}
-        <div className="flex flex-wrap w-full gap-3 justify-between">
+      <div className="px-5 pt-[60px]">
+        <div className="flex flex-col justify-between sm:flex-row">
+          <h1 className="py-3 text-5xl font-bold capitalize">
+            Games {name.replace(/-/g, " ")}
+          </h1>
+          <Filters
+            setDate={setDate}
+            setIsPending={setIsPending}
+            setOrdering={setOrdering}
+            setPage={setPage}
+          />
+        </div>
+        {isPending && <Loader />}
+        {data?.results?.length <= 0 && <NoResults/>}
+        <div className="flex flex-wrap w-full gap-3 justify-center md:justify-between py-10">
           {data?.results.map((game) => (
             <div
-              className="relative rounded-lg max-w-[350px] h-full min-w-[270px]  overflow-hidden "
+              className="relative w-full max-w-[170px]  md:max-w-[250px] h-full md:min-w-[250px] overflow-hidden hover:-translate-y-3 transition-transform duration-200"
               key={game.id}
             >
               <Card
-                capture={game.short_screenshots}
+                img={game.background_image}
                 name={game.name}
                 id={game.id}
                 genres={game.genres}
@@ -39,7 +56,15 @@ export const Genres = () => {
             </div>
           ))}
         </div>
+        <Pagination
+          next={data?.next}
+          previous={data?.previous}
+          page={page}
+          setIsPending={setIsPending}
+          setPage={setPage}
+        />
+        <Footer />
       </div>
-    </Layout>
+    </>
   );
 };
