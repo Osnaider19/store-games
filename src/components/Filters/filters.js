9f69138c0f1cc1bc49a4b7be5771165cc052d6fe
osnaider <osnaidermartinez2002@gmail.com>
2023-08-14 21:11:26 -0500

    add filter the ordering

---
+++ src/components/Filters/filters.js
@@ -0,0 +1,34 @@
+export const ordering = [
+  {
+    id: 1,
+    name: "name",
+    value: "name",
+  },
+  {
+    id: 2,
+    name: "released",
+    velue: "released",
+  },
+ 
+  {
+    id: 3,
+    name: "date added",
+    value: "-created",
+  },
+  {
+    id: 3,
+    name: "updated",
+    value: "-updated",
+  },
+  {
+    id: 4,
+    name: "average rating",
+    value: "-rating",
+  },
+  {
+    id: 5,
+    name: "release date",
+    value: "-released",
+  },
+ 
+];
