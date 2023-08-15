c32aaf893dc48810cb92c8577224cfe48a85cd94
osnaider <osnaidermartinez2002@gmail.com>
2023-08-14 21:41:51 -0500

    Loader card modific style

--- src/components/Loader/LoaderCard.jsx
+++ src/components/Loader/LoaderCard.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import "./index.css";
 export const LoaderCard = () => {
   return (
-    <div className="relative flex justify-between gap-5 items-center w-full">
+    <div className="relative flex flex-wrap justify-between gap-5 items-center w-full">
       <div>
-        <div className="min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
+        <div className="max-w-[200px] md:min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
         <div className="w-full h-[25px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[60%] h-[15px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[90%] h-[20px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[50%] h-[15px] rounded-sm mt-2 loader-animation "></div>
       </div>
       <div>
-        <div className="min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
+        <div className="max-w-[200px] md:min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
         <div className="w-full h-[25px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[60%] h-[15px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[90%] h-[20px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[50%] h-[15px] rounded-sm mt-2 loader-animation "></div>
       </div>
       <div>
-        <div className="min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
+        <div className="max-w-[200px] md:min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
         <div className="w-full h-[25px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[60%] h-[15px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[90%] h-[20px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[50%] h-[15px] rounded-sm mt-2 loader-animation "></div>
       </div>
       <div>
-        <div className="min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
+        <div className="max-w-[200px] md:min-w-[250px] w-full h-[205px] rounded-sm loader-animation"></div>
         <div className="w-full h-[25px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[60%] h-[15px] rounded-sm mt-2 loader-animation "></div>
         <div className="w-[90%] h-[20px] rounded-sm mt-2 loader-animation "></div>
