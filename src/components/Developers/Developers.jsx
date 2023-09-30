9418a09937f064bf853c4f206ca4f5c0ff2fc05f
osnaider <osnaidermartinez2002@gmail.com>
2023-09-30 15:47:50 -0500

    add pagination

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -4,10 +4,26 @@ import { Loader } from "../Loader/Loader";
 import { NoResults } from "../Games/NoResults";
 import { Footer } from "../Footer/Footer";
 import { useDevelopers } from "../../hooks/useDevelopers";
+import { useEffect } from "react";
+import { Loadmore } from "../buttons/Loadmore";
+import { LoaderFeching } from "../Loader/LoaderFeching";
 export const Developers = () => {
   const { name } = useParams();
-  const { games, isLoading, isError } = useDevelopers();
-  
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+    isFetchingNextPage,
+  } = useDevelopers();
+
+  useEffect(() => {
+    refetch();
+    scrollTo(0, 0);
+  }, [name]);
+  const games = data?.pages?.flatMap((page) => page.games) ?? [];
   return (
     <>
       <div className="pt-[60px] px-8 py-5">
@@ -36,7 +52,12 @@ export const Developers = () => {
             </div>
           ))}
         </div>
-
+        <div className="flex justify-center items-center w-full">
+          {hasNextPage && isFetchingNextPage === false && (
+          <Loadmore fetchNextPage={fetchNextPage} />)}
+          {hasNextPage === false && <p>no hay mas resultados</p>}
+          {isFetchingNextPage && <LoaderFeching />}
+        </div>
         <Footer />
       </div>
     </>
--- src/services/getDevelopers.jsx
+++ src/services/getDevelopers.jsx
@@ -1,28 +1,27 @@
 import { API_KEY, URL } from "../config/config";
-export async function getDevelopers({ pageParam = 1 , name}) {
-    try {
-      const res = await fetch(`${URL}/games?key=${API_KEY}&developers=${name}&page=${pageParam}`);
-      if (!res.ok) {
-        throw {
-          err: true,
-          status: res.status,
-          statusText: !res.statusText ? "Ocurrió un error" : res.statusText,
-        };
-      }
-      const data = await res.json();
-      const games = data.results;
-      const nextCursor = data.next
-        ? parseInt(data.next.match(/page=(\d+)/)[1])
-        : null;
-      return {
-        games,
-        nextCursor,
-      };
-    } catch (error) {
+export async function getDevelopers(pageParam , name) {
+  try {
+    const res = await fetch(
+      `${URL}/games?key=${API_KEY}&developers=${name}&page=${pageParam}`
+    );
+    if (!res.ok) {
       throw {
         err: true,
         status: res.status,
         statusText: !res.statusText ? "Ocurrió un error" : res.statusText,
       };
     }
+    const data = await res.json();
+    const games = data.results;
+    const nextCursor = data.next
+      ? parseInt(data.next.match(/page=(\d+)/)[1])
+      : null;
+    console.log(nextCursor);
+    return {
+      games,
+      nextCursor,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
