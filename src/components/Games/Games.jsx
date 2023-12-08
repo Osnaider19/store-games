6ac2e7d23016ba6d01c32009acc67f890c3b2c86
osnaider <osnaidermartinez2002@gmail.com>
2023-12-07 22:56:47 -0500

    add infiniti scroll

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -10,6 +10,7 @@ import { ErrorFeching } from "../Errors/ErrorFeching";
 import { Loadmore } from "../buttons/Loadmore";
 import { FiltersDate } from "../Filters/FiltersDate";
 import { useQueryClient } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 export const Games = () => {
   const queryClient = useQueryClient();
   const {
@@ -31,14 +32,18 @@ export const Games = () => {
     queryClient.removeQueries({ queryKey: ["games"], exact: true });
     reloadFirstPage();
   }, [ordering, date]);
+  const { ref, inView } = useInView();
   useEffect(() => {
-    scrollTo(0, 0);
-  }, []);
-  const games = data?.pages?.flatMap((page) => page.games) ?? [];
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
+  const games = data?.pages?.flatMap((page) => page.games) ?? [];
+  console.log(games);
   return (
     <>
-      <div className="px-4 py-4">
+      <div className="px-4 py-4 w-[80%] mx-auto">
         <div className="flex flex-col justify-between sm:flex-row">
           <h1 className="text-4xl w-full font-semibold py-4 sm:w-auto  md:text-6xl">
             All games
@@ -52,7 +57,7 @@ export const Games = () => {
         {isError && <ErrorFeching />}
         {games.length <= 0 && isLoading === false && <NoResults />}
         <div
-          className="flex flex-wrap justify-center gap-3 sm:justify-between pt-3"
+          className="flex flex-wrap justify-center gap-3 sm:justify-between pt-3 w-full mx-auto"
           id="mas"
         >
           {games?.map((game) => (
@@ -68,16 +73,20 @@ export const Games = () => {
           ))}
         </div>
         {hasNextPage && (
-          <div className="flex w-full justify-center items-center">
+          <div className="flex w-full justify-center items-center " ref={ref}>
             {isFetchingNextPage ? (
               <LoaderFeching />
             ) : (
-              <Loadmore fetchNextPage={fetchNextPage} />
+              <Loadmore fetchNextPage={fetchNextPage} ref={ref} />
             )}
           </div>
         )}
 
-        {hasNextPage === false && <span>no hay mas resultados </span>}
+        {hasNextPage === false && (
+          <div className="w-full flex justify-center items-center">
+            <span className="mx-auto">no hay mas resultados </span>
+          </div>
+        )}
         <Footer />
       </div>
     </>
--- src/components/Games/NoResults.jsx
+++ src/components/Games/NoResults.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const NoResults = () => {
   return (
-    <div className='flex justify-center items-center w-full'>
+    <div className='flex justify-center items-center w-full mx-auto  bg-red-500'>
         <span className='text-3xl font-semibold py-3'>No results please check filters</span>
     </div>
   )
